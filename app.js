12533743827d09c2f2c1f5380ad52f8444e4aa1c
theboringschool <theboringschool.org@gmail.com>
2022-05-21 17:47:14 +0545

    Added Cache

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const csrf = require("csurf");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+const NodeCache = require("node-cache");
+const customCache = new NodeCache();
 
 /**
  * * @setups => application setups
@@ -29,23 +32,44 @@ var parseForm = bodyParser.urlencoded({ extended: false });
  * @dev X-Powered-By for Security, Save Bandwidth in ExpressJS(Node.js)
  * */
 app.use(helmet());
-app.disable("x-powered-by");
+app.use(helmet.hidePoweredBy());
+app.use(helmet.noSniff());
 
+/**
+ * @dev Rate Limiting
+ */
+const apiLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 /**
  * * @routes => application routes
  */
 app
-  .get("/", csrfProtection, (req, res) => {
-    res.json({
-      msg: "Welcome to The Boring School's IP Address Banner API 🚀.",
-      csrfToken: req.csrfToken(),
-    });
+  .get("/", apiLimiter, csrfProtection, (req, res) => {
+    if (!customCache.has("csrfToken")) {
+      const csrfToken = req.csrfToken();
+
+      customCache.set("csrfToken", token, 10 * 60);
+
+      res.json({
+        msg: "Welcome to The Boring School's IP Address Banner API 🚀.",
+        csrfToken,
+      });
+    } else {
+      res.json({
+        msg: "Welcome to The Boring School's IP Address Banner API 🚀.",
+        csrfToken,
+      });
+    }
   })
   /**
    * @dev POST /ip => may raise Invalid CSRF Token error, solution explained
    * * Add header on client request
    * * {CSRF-Token = "Cross-site request forgery protection token"}
-   * * Note: "Call "/" to get the CSRF-Token" once and set to 
+   * * Note: "Call "/" to get the CSRF-Token" once and set to
    * * localStorage or Cookie in client side.
    */
   .post("/ip", parseForm, csrfProtection, (req, res) => {
